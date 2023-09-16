97fa94671c9b33e18f222fffae671760ae7336b1
AugustoWinkler <111334249+AugustoWinkler@users.noreply.github.com>
2023-09-16 20:12:08 -0300

    new

--- exercicios/aula16ex02/ex015/script.js
+++ exercicios/aula16ex02/ex015/script.js
@@ -22,8 +22,41 @@ function inLista(n, l) {
 
 function adicionar(){
     if (isNumero(num.value) && !inLista(num.value, valores)){ 
-        
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = (`Valor ${num.value} Adicionado.`)
+        lista.appendChild(item)
+        res.innerHTML = ''
 } else {
     window.alert('Valor Invalido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if (valores.length == 0){
+        window.alert('Adicione valores antes de Finalizar!')
+    }else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores){
+            soma+=valores[pos]
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if (valores[pos] < menor)
+            menor = valores[pos]
+        }
+        media = soma / tot
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O Maior valor informador foi ${maior} </p>`
+        res.innerHTML += `<p>O Menor valor informador foi ${menor} </p>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma} </p>`
+        res.innerHTML += `<p>A Média dos valores temos ${media} </p>`
     }
 }
