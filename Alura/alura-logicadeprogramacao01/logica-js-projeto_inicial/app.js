324ee174e4c58ad70758e3fcd58bd84f37de2020
AugustoWinkler <111334249+AugustoWinkler@users.noreply.github.com>
2023-11-29 17:40:06 -0300

    Update app.js

--- Alura/alura-logicadeprogramacao01/logica-js-projeto_inicial/app.js
+++ Alura/alura-logicadeprogramacao01/logica-js-projeto_inicial/app.js
@@ -1,14 +1,26 @@
 alert('Boas Vindas ao jogo do número secreto!');
-let numeroSecreto = 29;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1
 
-if (numeroSecreto==chute){
-    alert(`Isso ai você acertou o numero secreto! ${numeroSecreto}`);
+// Enquanto
+while(chute!=numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 10');
+    if (chute==numeroSecreto){
+        alert(`Isso ai você acertou o numero secreto! ${numeroSecreto} com ${tentativas} tentativas`);
+    }
+    else{
+        if(chute<numeroSecreto){
+            alert(`O Número Secreto é maior que ${chute}`);
+        }else{
+            alert(`O Numero Secreto é menor que ${chute}`);
+        }
+        tentativas++
+    }    
 }
-else{
-    alert(`Você errou o número secreto era: ${numeroSecreto}`)
 
-}
+
+
 
 
